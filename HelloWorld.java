f731d39d8235c80e780815cfb04033b5c39c21c1
LICONG2022 <125644672+LICONG2022@users.noreply.github.com>
2024-04-16 14:17:04 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld{
 public static void main(String[] args){
 	System.out.println("Hello world");
-    System.out.println("远程仓库");
+        System.out.println("远程仓库");
+	System.out.println("2024年4月16日");
 
 }
 
